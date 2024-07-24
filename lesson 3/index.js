364d42e7b1b17ec30722d7d1df58d122dbb05896
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2024-07-24 13:00:14 +0500

    if else, nullish

---
+++ lesson 3/index.js
@@ -0,0 +1,97 @@
+// let temp = "sovuq"; // issiq, sovuq, yomgir, qor
+
+// 0 , null, undefined
+// if (temp === "issiq") {
+//   console.log("Havo issiq, yengil kiyinib oling");
+// } else if (temp === "sovuq") {
+//   console.log("Havo sovuq, qalin kiyinib oling");
+// } else if (temp === "yomgir") {
+//   console.log("Havo yomgir, soyabon olib oling");
+// } else if (temp === "bulut") {
+//   console.log("Havo qor, qalin kiyinib oling, soyabon oling");
+// } else {
+//   console.log("nomalum ob havo");
+// }
+
+// if (temp === "sovuq") {
+//   console.log("nomalum ob havo, sovuq");
+// }
+
+// temp = 12; // temp >0 && temp < 10, temp >10 && temp<15, temp >15 && temp<25
+
+// if (temp < 0) {
+//   console.log("havo juda sovuq");
+// } else if (temp < 10) {
+//   console.log("havo  sovuq");
+// } else if (temp < 15) {
+//   console.log("havo issiq");
+// } else if (temp < 25) {
+//   console.log("havo juda issiq");
+// }
+
+// temp = 5;
+// if (temp < 0) {
+//   console.log("havo juda sovuq");
+// } else if (temp < 25) {
+//   console.log("havo juda issiq");
+// } else if (temp < 15) {
+//   console.log("havo issiq");
+// } else if (temp < 10) {
+//   console.log("havo  sovuq");
+// }
+
+// temp = 25; // temp >0 && temp < 10, temp >10 && temp<15, temp >15 && temp<25
+
+// if (temp < 0) {
+//   console.log("havo juda sovuq");
+// } else if (temp <= 25 && temp > 15) {
+//   console.log("havo juda issiq");
+//   console.log("havo juda issiq");
+// } else if (temp <= 15 && temp > 10) {
+//   console.log("havo issiq");
+// } else if (temp <= 10 && temp > 0) {
+//   console.log("havo  sovuq");
+// } else {
+//   console.log("not found");
+// }
+
+// if('condition'){'code'}
+// else if('condition'){'code'}
+// else{'code'}
+
+// if ("condition") console.log("havo juda issiq");
+// else if ("condition") return true;
+// else return false;
+
+// temp >0 && temp < 10, temp >10 && temp<15, temp >15 && temp<25
+
+// if (temp < 0) console.log("havo juda sovuq");
+// else if (temp <= 25 && temp > 15) console.log("havo juda issiq");
+// else if (temp <= 15 && temp > 10) console.log("havo issiq");
+// else if (temp <= 10 && temp > 0) console.log("havo  sovuq");
+// else console.log("not found");
+
+// ? :
+
+// // temp < 0? === if (temp < 0)
+// temp < 0
+//   ? console.log("havo juda sovuq")
+//   : temp < 10
+//   ? console.log("havo sovuq")
+//   : temp < 15
+//   ? console.log("havo issiq")
+//   : console.log("havo juda issiq");
+
+// Nullish ?? -> null undefined
+// OR || -> null undefined, 0 , false, NaN, "", -1 0n
+
+// console.log(false || 1);
+// console.log(false ?? 1);
+
+// console.log(NaN || 1);
+// console.log(NaN ?? 1);
+// console.log(null || 1);
+// console.log(null ?? 1);
+
+// console.log(undefined || 1);
+// console.log(undefined ?? 1);
